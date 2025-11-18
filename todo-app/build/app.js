e2b93203bcdf9a72190df8793852fc852f327301
Neema Mwende <gracepete12@gmail.com>
2025-11-18 13:38:26 +0300

    refactored

---
+++ todo-app/build/app.js
@@ -0,0 +1,20 @@
+import { __jacJsx, __jacSpawn } from "@jac-client/utils";
+function TodoItem(props) {
+  return __jacJsx("div", {}, [__jacJsx("input", {
+    "type": "checkbox",
+    "checked": props.done
+  }, []), __jacJsx("span", {}, [props.text]), __jacJsx("button", {}, ["Delete"])]);
+}
+function app() {
+  return __jacJsx("div", {}, [__jacJsx("h1", {}, ["My Todos"]), __jacJsx(TodoItem, {
+    "text": "Learn Jac basics",
+    "done": false
+  }, []), __jacJsx(TodoItem, {
+    "text": "Build a todo app",
+    "done": false
+  }, []), __jacJsx(TodoItem, {
+    "text": "Deploy to production",
+    "done": true
+  }, [])]);
+}
+export { TodoItem, app };
